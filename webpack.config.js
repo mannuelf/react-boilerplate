5214ec990f0f456d319fed0d0cc50f6990d10f56
mannuelf <mannuel.ferreira@gmail.com>
2019-11-23 11:31:26 +0100

    created webpack config to sue babel-presets-env

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require("path");
 
 module.exports = {
-    mode: "production",
+    mode: "production", // set mode, development or production, can be set dynamically.
     entry: "./src/index.js",
     output: {
         path: path.join(__dirname, "dist"), // absolute path to output
         filename: "app.bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                options: {
+                    presets: ["@babel/preset-env"] // transforms modern ES6 to older version in app.bundle
+                }
+            }
+        ]
     }
 }
