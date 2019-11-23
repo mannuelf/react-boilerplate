22ffb905442eb4ff7376c90834b7e44cadc39f96
mannuelf <mannuel.ferreira@gmail.com>
2019-11-23 11:40:18 +0100

    added react app code and babel-react preset so that babel can understand and compile JSX

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,10 @@ module.exports = {
                 loader: "babel-loader",
                 exclude: /node_modules/,
                 options: {
-                    presets: ["@babel/preset-env"] // transforms modern ES6 to older version in app.bundle
+                    presets: [
+                        "@babel/preset-env",
+                        "@babel/preset-react"
+                    ] // transforms modern ES6 to older version in app.bundle
                 }
             }
         ]
