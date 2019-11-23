6e524eb192aa169ed844d408019f9d2c2ec7320a
mannuelf <mannuel.ferreira@gmail.com>
2019-11-23 11:46:49 +0100

    created new template file in src and added is a template into the constructor function htmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -23,5 +23,9 @@ module.exports = {
             }
         ]
     },
-    plugins: [ new HtmlWebpackPlugin() ]
+    plugins: [ 
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
 }
